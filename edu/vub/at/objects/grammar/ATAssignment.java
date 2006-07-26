a841fa07ccb5a0dd3958b539d44dff8f4108c0fd
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2006-07-26 14:08:46 +0000

    Added an initial interface to all AG elements.
    git-svn-id: https://ambienttalk.googlecode.com/svn/interpreter/trunk@17 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

---
+++ edu/vub/at/objects/grammar/ATAssignment.java
@@ -0,0 +1,39 @@
+/**
+ * AmbientTalk/2 Project
+ * ATAssignment.java created on 26-jul-2006 at 14:54:16
+ * (c) Programming Technology Lab, 2006 - 2007
+ * Authors: Tom Van Cutsem & Stijn Mostinckx
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package edu.vub.at.objects.grammar;
+
+import edu.vub.at.objects.ATAbstractGrammar;
+
+/**
+ * @author tvc
+ *
+ * The common interface to all assignment AG nodes.
+ */
+public interface ATAssignment extends ATAbstractGrammar {
+
+}
