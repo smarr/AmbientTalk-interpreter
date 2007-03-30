c4402609cbf93d1248032c09692e1bd6bc783804
tvcutsem <tvcutsem@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2007-03-30 14:53:30 +0000

    Reworked the logging framework to use a simpler configuration file, and to configure the loggers by hand
    git-svn-id: https://ambienttalk.googlecode.com/svn/interpreter/trunk@440 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

--- src/edu/vub/at/actors/net/Logging.java
+++ src/edu/vub/at/actors/net/Logging.java
@@ -27,8 +27,14 @@
  */
 package edu.vub.at.actors.net;
 
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.Priority;
 
 /**
  * Auxiliary class used to group Log4J loggers.
@@ -64,8 +70,36 @@ public final class Logging {
 	
 	
 	static {
-		// intialize the Log4J API
-		PropertyConfigurator.configure(Logging.class.getResource("logging.props"));
+		// intialize the Log4J API and the loggers
+		
+		// see http://logging.apache.org/log4j/docs/api/org/apache/log4j/PatternLayout.html
+		// for a listing of possible pattern variables
+		// d = date, t = thread, p = priority, c = category, m = message, n = newline
+		ConsoleAppender console = new ConsoleAppender(new PatternLayout("%d %-5p %c - %m%n"), "System.err");
+		EventLoop_LOG.addAppender(console);
+		Actor_LOG.addAppender(console);
+		RemoteRef_LOG.addAppender(console);
+		VirtualMachine_LOG.addAppender(console);
+		Init_LOG.addAppender(console);
+		
+		Logger jGroupsLogger = Logger.getInstance("org.jgroups");
+		jGroupsLogger.addAppender(console);
+		
+		Properties props = new Properties();
+		try {
+			props.load(Logging.class.getResourceAsStream("logging.props"));
+			Enumeration allProperties = props.propertyNames();
+			// interpret each KEY=VALUE pair in the log file as
+			// Logger(KEY).setPriority(VALUE)
+			// where KEY = the name of a logger and VALUE = a priority level (e.g. WARN, DEBUG, ...)
+			while (allProperties.hasMoreElements()) {
+				String loggerName = (String) allProperties.nextElement();
+				Logger.getInstance(loggerName).setPriority(Priority.toPriority(props.getProperty(loggerName)));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//PropertyConfigurator.configure(Logging.class.getResource("logging.props"));
 	}
 	
 }
