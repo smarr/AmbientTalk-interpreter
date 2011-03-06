2a36284470ab879abfcfd4f65f9f6755beb1b3ed
egonzalezb <egonzalezb@c0a7138a-1c4d-0410-81cb-3328cd1d7e1d>
2011-03-06 21:05:43 +0000

    -Fixing a bug: source location information of an asynchronous message which is annotated was not set on the original message.
    git-svn-id: https://ambienttalk.googlecode.com/svn/interpreter/trunk@2711 c0a7138a-1c4d-0410-81cb-3328cd1d7e1d

--- src/edu/vub/at/objects/natives/grammar/AGAsyncMessageCreation.java
+++ src/edu/vub/at/objects/natives/grammar/AGAsyncMessageCreation.java
@@ -54,7 +54,8 @@ public class AGAsyncMessageCreation extends AGMessageCreation implements ATAsync
 	protected ATMessage createMessage(ATContext ctx, ATSymbol selector, ATTable evaluatedArgs, ATTable annotations) throws InterpreterException {
 		// dirty trick to pass source location of the AGASyncMessageCreation to the actorMirror.
 		// this table is anycase created by the interpreter.
-		evaluatedArgs.impl_setLocation(this.impl_getLocation());
+		
+		annotations.impl_setLocation(this.impl_getLocation());
 		return OBJLexicalRoot._INSTANCE_.base_reflectOnActor().base_createMessage(selector, evaluatedArgs, annotations);
 
 	}
